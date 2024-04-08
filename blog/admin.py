1ea9ebab88b9749f98bd77e5b5badceb97b278da
Rodney Rivera <rodneyrivera@MacBook-Pro-de-Rodney.local>
2024-04-08 14:12:33 -0300

    Meus arquivos do dg

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ("author", "title", "text",
+                    "created_date", "published_date")
